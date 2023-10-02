6ce36268ba1b4400ca83c32659027f488e074578
Darjan Hren <6051129+darisi@users.noreply.github.com>
2023-10-02 12:31:02 +0200

    Commit from TeleportHQ

--- pages/index.js
+++ pages/index.js
@@ -35,14 +35,28 @@ const Home = (props) => {
             Innovation
           </h1>
           <span className="home-text1">
-            <span className="home-text2">E:</span>
-            <span> ???@envirodial.com</span>
+            Naša spletna stran je v izdelavi in bo kmalu spet na voljo.
           </span>
-          <span className="home-text4">
-            <span className="home-text5">T:</span>
-            <span> +386(0)123 1234</span>
+          <div className="home-container3">
+            <span className="home-text2">
+              <span className="home-text3">E:</span>
+              <span>
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+            </span>
+            <a href="mailto:info@envirodual.com?subject=" className="home-link">
+              info@envirodual.com
+            </a>
+          </div>
+          <span className="home-text5">
+            <span className="home-text6">T:</span>
+            <span> +386(0)41 343 932</span>
           </span>
-          <span className="home-text7">
+          <span className="home-text8">
             Envirodual d.o.o., Tepanje 28D, 3210 Sl. Konjice, Slovenija
           </span>
         </div>
@@ -102,17 +116,42 @@ const Home = (props) => {
             text-decoration: none;
           }
           .home-text1 {
+            color: #90ca73;
+            font-size: 14px;
+            margin-top: 36px;
+            text-align: center;
+            font-family: Poppins;
+            padding-top: 12px;
+            padding-left: 24px;
+            border-radius: 12px;
+            margin-bottom: 36px;
+            padding-right: 24px;
+            padding-bottom: 12px;
+            text-transform: none;
+            text-decoration: none;
+            background-color: #ffffff;
+          }
+          .home-container3 {
+            flex: 0 0 auto;
+            width: 100%;
+            height: auto;
+            display: flex;
+            align-items: flex-start;
+            justify-content: center;
+          }
+          .home-text2 {
             color: rgb(255, 255, 255);
             margin-top: 24px;
             text-align: center;
             font-family: Inter;
+            margin-right: 8px;
             text-transform: none;
             text-decoration: none;
           }
-          .home-text2 {
+          .home-text3 {
             font-weight: 700;
           }
-          .home-text4 {
+          .home-link {
             color: rgb(255, 255, 255);
             margin-top: 24px;
             text-align: center;
@@ -121,9 +160,17 @@ const Home = (props) => {
             text-decoration: none;
           }
           .home-text5 {
+            color: rgb(255, 255, 255);
+            margin-top: 8px;
+            text-align: center;
+            font-family: Inter;
+            text-transform: none;
+            text-decoration: none;
+          }
+          .home-text6 {
             font-weight: 700;
           }
-          .home-text7 {
+          .home-text8 {
             color: rgb(255, 255, 255);
             font-size: 14px;
             margin-top: 48px;
@@ -156,7 +203,7 @@ const Home = (props) => {
               font-size: 18px;
             }
           }
-          @media (max-width: 479px) {
+          @media (max-width: 375px) {
             .home-container {
               padding-left: 0px;
               padding-right: 0px;
